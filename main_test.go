e1be44e8edb1768021680c1a34d9359544e6c1da
Marcel Thiele <marcel.thiele55@gmail.com>
2022-03-16 12:21:33 +0100

    working on tests

--- main_test.go
+++ main_test.go
@@ -1,11 +1,31 @@
 package main_test
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
 
-func TestMain(t *testing.T) {
-	got := 1
-	want := 1
-	if got != want {
-		t.Errorf("got %v, want %v", got, want)
-	}
+	_ "github.com/leplasmo/ascetic"
+)
+
+// func performRequest(r http.Handler, method, path string) *httptest.ResponseRecorder {
+// 	req, _ := http.NewRequest(method, path, nil)
+// 	w := httptest.NewRecorder()
+// 	r.ServeHTTP(w, req)
+// 	return w
+// }
+
+func TestTodoCRUD(t *testing.T) {
+	th := newTodoHandler()
+	t.Run("Retrieve non-existing ID", func(t *testing.T) {
+		// w := performRequest(router, "GET", "/todos/-2")
+		// assert.Equal(t, http.StatusBadRequest, w.Code)
+		// assert.Equal(t, "{\"error\":\"Record not found!\"}", w.Body.String())
+		wr := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodGet, "/todos", nil)
+		th.get(wr, req)
+		if wr.Code != http.StatusOK {
+			t.Errorf("got HTTP status code %d, expected 200", wr.Code)
+		}
+	})
 }
